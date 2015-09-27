7d8f1153a087ae53fbec7a2e2d997b2b0cc5c5d6
Nancy Wang <n0w1253@gmail.com>
2015-09-27 15:20:51 -0600

    modified

--- public/TestUIRouter/app.js
+++ public/TestUIRouter/app.js
@@ -56,17 +56,45 @@ routerApp.controller('scotchController', function ($scope) {
     $scope.scotches = [
         {
             name: 'Macallan 12',
-            price: 50
+            price: 50,
+            selected: false
         },
         {
             name: 'Chivas Regal Royal Salute',
-            price: 10000
+            price: 10000,
+            selected: false
         },
         {
             name: 'Glenfiddich 1937',
-            price: 20000
+            price: 20000,
+            selected: false
         }
     ];
+
+    // This property is bound to the checkbox in the table header
+    $scope.allItemsSelected = false;
+
+// Fired when an entity in the table is checked
+    $scope.selectEntity = function () {
+        // If any entity is not checked, then uncheck the "allItemsSelected" checkbox
+        for (var i = 0; i < $scope.scotches.length; i++) {
+            if (!$scope.scotches[i].isChecked) {
+                $scope.allItemsSelected = false;
+                return;
+            }
+        }
+
+        // ... otherwise ensure that the "allItemsSelected" checkbox is checked
+        $scope.allItemsSelected = true;
+    };
+
+    // Fired when the checkbox in the table header is checked
+    $scope.selectAll = function () {
+        // Loop through all the entities and set their isChecked property
+        for (var i = 0; i < $scope.scotches.length; i++) {
+            $scope.scotches[i].isChecked = model.allItemsSelected;
+        }
+    };
 
 });
 
